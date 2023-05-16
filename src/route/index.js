f87176abacb306334bc46b8880a9d2cd8adef011
Natalya2121 <andriyenkonatalya@gmail.com>
2023-05-16 17:31:06 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,34 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: "Ivan",
+    lastname: "Ivanov",
+  },
+  position: "Junior Fullstack JS Developer",
+  salary: "600$ в місяць",
+  address: "Ukraine, Lviv",
+}
+
+var footer = {
+  social: {
+    email: {
+      text: "dmytro@mail.com",
+      href: "mailto:dmytro@mail.com",
+    },
+    phone: {
+      text: "+380670000123",
+      href: "tel:+380670000123",
+    },
+    linkedin: {
+      text: "LinkedIn",
+      href: "https://www.linkedin.com/in/dmytro-test",
+    },
+  },
+}
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -22,11 +50,134 @@ router.get('/', function (req, res) {
 router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
-    // ↙ сюди вводимо JSON дані
+    pege: {
+      title: "Resume | Summary",
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: "Summary",
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+              work on a new project I learn the domain and try to understand the idea of the project. Good team
+              player, every colleague is a friend to me.`,
+      },
+
+      experience: {
+        title: "Other experience",
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+              tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+              for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
+
   })
 })
 
 // ================================================================
 
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    pege: {
+      title: "Resume | Skills",
+    },
+
+    header,
+
+    main: {
+      skills: [
+        { name: "HTML", point: 10, isTop: true },
+        { name: "Handlebars", point: 10, isTop: true },
+        { name: "VS Code", point: 9 },
+        { name: "Git", point: 9 },
+        { name: "Terminal & NPM", point: 8 },
+        { name: "React.js", point: 0 },
+        { name: "PHP", point: null },
+      ],
+      hobbies: [
+        { name: "Reading", isMain: true },
+        { name: "Painting", isMain: false },
+        { name: "Cooking", isMain: false },
+      ]
+    },
+
+    footer,
+
+  })
+})
+
+
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    pege: {
+      title: "Resume | Education",
+    },
+
+    header,
+
+    main: {
+      educations: [
+        { name: "Franko's Lviv National University", isEnd: true },
+        { name: "Kyiv University of Culture", isEnd: false },
+      ],
+      certificates: [
+        { name: "Сertificate 1", id: 1 },
+        { name: "Сertificate 2", id: 2 },
+        { name: "Сertificate 3", id: 3 },
+      ],
+    },
+
+    footer,
+
+  })
+})
+
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    layout: "big",
+    pege: {
+      title: "Resume | Work",
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: "Junior Fullstack Developer",
+          company: { name: "IT Brains", url: null },
+          duration: { from: "10.10.2022", to: null },
+          projectAmount: 1,
+          projects: [{
+            name: "Resume",
+            url: "https://it-brains.com.ua/",
+            about: "This returns the average CLR time, the total CLR time",
+            stacks: [
+              { name: "React.js" },
+              { name: "HTML / CSS" },
+              { name: "Nodejs" },
+            ],
+            awards: [
+              { name: "The Package object is the top-level container" },
+              { name: "Tasks perform most of the work performed by a package" },
+            ],
+            stackAmount: 3,
+            awardAmount: 2,
+          }
+          ],
+        },
+      ],
+    },
+
+    footer,
+
+  })
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
